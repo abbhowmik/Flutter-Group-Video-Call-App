7cbb7609e99d1ac4b684fffe3757f7eb1bf7f01c
Ashis Bhowmik <ashisbhowmikisp@gmail.com>
2021-11-29 21:41:12 +0530

    1st build complete without authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,19 @@
+// @dart=2.9
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:splashscreen/splashscreen.dart';
 import 'package:video_call/pages/home_page.dart';
 
 void main() {
   SystemChrome.setSystemUIOverlayStyle(
-    SystemUiOverlayStyle(
+    const SystemUiOverlayStyle(
       statusBarColor: Colors.transparent,
     ),
   );
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -23,7 +23,30 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(),
+      home: SpScrn(),
+    );
+  }
+}
+
+class SpScrn extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SplashScreen(
+        seconds: 2,
+        navigateAfterSeconds: const HomePage(),
+        image: Image.asset("assets/logo.png"),
+        title: Text(
+          "Welcome to MeetIn",
+          style: TextStyle(
+            color: Colors.green,
+            fontSize: 15,
+          ),
+        ),
+        photoSize: 50,
+        styleTextUnderTheLoader: const TextStyle(),
+        loaderColor: Colors.green,
+      ),
     );
   }
 }
